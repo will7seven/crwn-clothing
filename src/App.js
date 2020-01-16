5393be57d97dd1202efaf1b1dfc76002da5bd2ba
will7seven <will7seven@gmail.com>
2020-01-16 18:14:39 -0300

    added custom input and button components to sigin form

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact={true} path={'/'} component={HomePage} />
-        <Route path={'/shop'} component={ShopPage} />
+        <Route exact={true} path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );

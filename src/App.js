53972bc195a278c0a3dc4a664d5edd83d9f4a008
will7seven <will7seven@gmail.com>
2020-01-19 19:02:36 -0300

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -23,17 +23,12 @@ class App extends React.Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
             },
-            () => {
-              console.log(this.state);
-            },
-          );
+          });
         });
       }
       this.setState({ currentUser: userAuth });
